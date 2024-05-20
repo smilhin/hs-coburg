947a256db783a15bbf3db709b939696e6194a196
Smilhin Danyil <smilgin64.25@gmail.com>
2024-05-20 14:42:50 +0200

    Uebung7.2 (not finished yet)

---
+++ Prog2/Uebungsblatt/Uebung7/Uebung7.2/BibliothekTest.java
@@ -0,0 +1,29 @@
+import java.util.Collection;
+import java.util.Comparator;
+
+public class BibliothekTest {
+
+    public static void init(BibliothekList bib) {
+
+        bib.einfuegen(new Buch("9784898645133","Reinhard Schiedermeier", "Java Praktikum",                                                           23.0f));
+        bib.einfuegen(new Buch("01303451517",  "Harvey Deitel",          "How to program Java",                                                      134.0f));
+        bib.einfuegen(new Buch("0136290310",   "Bertrand Meyer",         "Object Oriented Software Construction",                                    67.0f));
+        bib.einfuegen(new Buch("9783642018558","Bertrand Meyer",         "Software Engineering Approaches for Offshore and Outsourced Development",  78.0f));
+        bib.einfuegen(new Buch("9783897214484","Kathy Sierra",           "Java von Kopf bis Fuss",                                                   110.0f));
+        bib.einfuegen(new Buch("9784898645133","Reinhard Schiedermeier", "Java Praktikum",                                                           23.0f));
+        bib.einfuegen(new Buch("01303451517",  "Harvey Deitel",          "How to program Java",                                                      134.0f));
+        bib.einfuegen(new Buch("0136290310",   "Bertrand Meyer",         "Object Oriented Software Construction",                                    67.0f));
+        bib.einfuegen(new Buch("9783642018558","Bertrand Meyer",         "Software Engineering Approaches for Offshore and Outsourced Development",  78.0f));
+        bib.einfuegen(new Buch("9783897214484","Kathy Sierra",           "Java von Kopf bis Fuss",                                                   110.0f));
+        bib.einfuegen(new Buch("9780123725011","Mark Utting",           "Pratical Model based Testing",                                                   189.0f));
+
+    }
+
+    class BuchNachTitel {
+        public int compareTo(Buch b2) {
+            for(Buch b1 : ) {
+
+            }
+        }
+    }
+}
