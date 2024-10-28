be4d5924335b1d22a34136bbede544d36bde480e
Smilhin Danyil <smilgin64.25@gmail.com>
2024-10-28 14:27:21 +0100

    pow java update

--- Sem3/ADs/Übung3/Pow.java
+++ Sem3/ADs/Übung3/Pow.java
@@ -2,26 +2,29 @@ public class Pow {
 
     public static void main(String[] srg) {
 
+        System.out.println(pow(2, 6));
+        System.out.println(pow(3, 4));
         System.out.println(pow(10, 5));
+        System.out.println(pow(4, 3));
 
     }
 
-    public static long pow(long x, int n){
+    public static long pow(long x, int n) {
         long result = 1;
         if(n % 2 == 0) {
-            result *= x;
-            while(n / 2 > 0) {
+
+            for(int i = 0; i < n / 2; i++) {
                 result *= x;
-                n--;
             }
-        }
-        else {
-            while((n - 1) / 2 > 0) {
-                result *= x;
-                n--;
-            }
+
             result *= result;
-            result *= x;
+        }
+
+        else {
+            for(int i = 0; i < (n - 1) / 2; i++) {
+                result *= x;
+            }
+            result *= result * x;
         }
 
         return result;
